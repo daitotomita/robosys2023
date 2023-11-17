6f5dc3fa76ad3a2bd5dd3b8c91823231c3fb3bd3
Tomita <benzen_0829@outlook.jp>
2023-11-17 21:43:35 +0900

    add test 2

--- test.bash
+++ test.bash
@@ -11,8 +11,22 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = "15 : -15 : 120 : 0.0083333333" ] || ng ${LINENO}
 
-[ "${out}" = "15 : -15 : 120 : 0.00833" ] || ng ${LINENO}
+out=$(seq 10 | ./plus)
+[ "${out}" = "55 : -55 : 3628800 : 2.756e-07" ] || ng ${LINENO}
+
+out=$(seq 1.0 | ./plus)
+[ "${out}" = "1 : -1 : 1 : 1.0" ] || ng ${LINENO}
+
+out=$(seq 1.1 | ./plus)
+[ "${out}" = "1 : -1 : 1 : 1.0" ] || ng ${LINENO}
+
+out=$(seq 1.9 | ./plus)
+[ "${out}" = "1 : -1 : 1 : 1.0" ] || ng ${LINENO}
+
+out=$(seq 2.0 | ./plus)
+[ "${out}" = "3 : -3 : 2 : 0.5" ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
