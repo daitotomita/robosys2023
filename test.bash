783521a080be0a782f9e10a4c3fba85a46996c11
Tomita <benzen_0829@outlook.jp>
2023-11-17 20:59:29 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -17,13 +17,13 @@ out=$(seq 5 | ./plus)
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 
-[ "$?" = 1 ]      || ng ${LINEMO}
-[ "${out}" = "" ] || ng ${LINEMO}
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
 
-[ "$?" = 1 ]      || ng ${LINEMO}
-[ "${out}" = "" ] || ng ${LINEMO}
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
