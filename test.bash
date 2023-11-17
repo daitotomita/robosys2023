f76b8cd0cce0182e71ad647d85f855b15d0a1fb9
Tomita <benzen_0829@outlook.jp>
2023-11-17 21:53:06 +0900

    add test 5

--- test.bash
+++ test.bash
@@ -54,6 +54,11 @@ out=$(echo % | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo ? | ./plus)
+
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 
 exit $res
